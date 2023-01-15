9682afe3254ed64b4573fef989392b5a502ecc76
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-15 19:25:05 +0530

    redux provider configured

--- cabx/App.js
+++ cabx/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Provider store={store}>
+      <View style={styles.container}>
       <Text>Cabx</Text>
-      <StatusBar style="auto" />
-    </View>
+      </View>
+    </Provider>
+    
   );
 }
 
