a0b27bf68e1f6e5398dd6a34ebd8fc80fdf8dc5f
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-16 12:07:37 +0530

    Homescreen, react icons, safe area configured

--- cabx/screens/HomeScreen.js
+++ cabx/screens/HomeScreen.js
@@ -1,17 +1,23 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-cyan h-full`}>
         <View style={tw `p-4`}>
-            <Image source={{
-                height: 100, width: 100, resizeMode: 'contain',
-                uri: 'https://as1.ftcdn.net/v2/jpg/04/18/01/54/1000_F_418015405_gym5TXugGl1D90UTf9wmb86n9JuddmkA.jpg',
+            <Image 
+            style={{
+                height: 100,
+                width: 100,
+                resizeMode: "contain",
             }}
-        />
-        <Text style={[tw `text-black-400 p-8 `, styles.text]}>Home</Text>
+            source={{
+                uri: "https://as1.ftcdn.net/v2/jpg/04/18/01/54/1000_F_418015405_gym5TXugGl1D90UTf9wmb86n9JuddmkA.jpg",
+            }}
+            />
+        <NavOptions/>
         </View>
     </SafeAreaView>
   )
