aab6415409a3cc7539cf3cf109a23415a58f2ac6
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-18 21:13:43 +0530

    Google map apis configured

--- cabx/screens/HomeScreen.js
+++ cabx/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import NavBar from '../components/NavBar'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_KEY } from '@env';
 
 const HomeScreen = () => {
   return (
@@ -16,6 +18,34 @@ const HomeScreen = () => {
             source={{
                 uri: "https://as1.ftcdn.net/v2/jpg/04/18/01/54/1000_F_418015405_gym5TXugGl1D90UTf9wmb86n9JuddmkA.jpg",
             }}
+            />
+
+        <GooglePlacesAutocomplete
+            placeholder='From'
+            styles={{
+                container: {
+                    flex: 0,
+                },
+                textInput: {
+                    fontSize: 15,
+                },
+            }}
+            onPress={(data, details = null) => {
+                console.log(data, details);
+            }}
+            fetchDetails={true}
+            returnKeyType={'search'}
+            minLength={1}
+
+
+            query={{    
+                key: GOOGLE_MAPS_KEY,
+                language: 'en',
+            }}
+
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+            
             />
         <NavBar/>
         </View>
