e32cbe1038559005d0845c78e20903c5b3030d72
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-19 19:51:21 +0530

    Update Map.js

--- cabx/components/Map.js
+++ cabx/components/Map.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapView from 'react-native-maps'
 import tw from 'tailwind-react-native-classnames'
+import { selectOrigin } from '../slices/navSlice';
+import { useSelector } from 'react-redux';
+
 const Map = () => {
   const origin = useSelector(selectOrigin);
 
@@ -15,7 +18,20 @@ const Map = () => {
             latitudeDelta: 0.006,
             longitudeDelta: 0.006,
         }}
-    />
+    >
+        {origin?.location && (
+            <Marker
+                coordinate={{
+                    latitude: origin.location.lat,
+                    longitude: origin.location.lng,
+                }}
+                title="Origin"
+                description={origin.description}
+                identifier="origin"
+            />
+        )}
+
+    </MapView>
     
 
   )
